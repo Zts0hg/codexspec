7372ed6f635a7703c3e8acd4347b5a83b02b6bbb
Aki <zhangzhaobin1025@gmail.com>
2026-08-29 18:47:22 +0800

    chore(release): bump version to v0.7.13

--- src/codexspec/__init__.py
+++ src/codexspec/__init__.py
@@ -44,7 +44,7 @@ from .profile import ensure_profile_scaffold, inject_profile_block
 from .translator import SUPPORTED_LANGUAGES, translate
 
 # Version info
-__version__ = "0.7.12"
+__version__ = "0.7.13"
 __author__ = "CodexSpec Team"
 
 # Constitution file path constants
